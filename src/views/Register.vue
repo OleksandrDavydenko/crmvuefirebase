b2199872c60f2481aa38f772b109dfd50113bb60
DavydHub <oleksandr.davydenko3@gmail.com>
2020-05-11 13:19:01 +0300

    register in firebase + errors

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -100,7 +100,7 @@ export default {
     agree: {checked: v => v}
   },
   methods: {
-    submitHandler () {
+    async submitHandler () {
       if (this.$v.$invalid) {
         this.$v.$touch()
         // eslint-disable-next-line no-useless-return
@@ -112,9 +112,10 @@ export default {
         password: this.password,
         name: this.name
       }
-      console.log(formData)
-
-      this.$router.push('/')
+      try {
+        await this.$store.dispatch('register', formData)
+        this.$router.push('/')
+      } catch (e) {}
     }
   }
 }
